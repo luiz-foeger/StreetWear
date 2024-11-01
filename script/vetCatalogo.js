2617fbf1a9861e91ef00279994511e87ba62d0b2
Luiz Felipe Föeger <luizffsan5@gmail.com>
2024-10-31 21:53:57 -0300

    Alterações no HTML e CSS
    Alterações no HTML

--- script/vetCatalogo.js
+++ script/vetCatalogo.js
@@ -1,10 +1,11 @@
-export const vetCatalogo = [
+export const vetCamisetas = [
     {
         id: "1",
         nome: 'CAMISETA "THINK" HIGH',
         preco: 142,
         imagem: 'camiseta-1.png',
         moletom: false,
+        modelo: 'camiseta'
     },
     {
         id: "2",
@@ -12,6 +13,7 @@ export const vetCatalogo = [
         preco: 142,
         imagem: 'camiseta-2.png',
         moletom: false,
+        modelo: 'camiseta'
     },
     {
         id: "3",
@@ -19,6 +21,7 @@ export const vetCatalogo = [
         preco: 142,
         imagem: 'camiseta-3.png',
         moletom: false,
+        modelo: 'camiseta'
     },
     {
         id: "4",
@@ -26,6 +29,7 @@ export const vetCatalogo = [
         preco: 153,
         imagem: 'camiseta-4.png',
         moletom: false,
+        modelo: 'camiseta'
     },
     {
         id: "5",
@@ -33,6 +37,7 @@ export const vetCatalogo = [
         preco: 168,
         imagem: 'camiseta-5.png',
         moletom: false,
+        modelo: 'camiseta'
     },
     {
         id: "6",
@@ -40,13 +45,15 @@ export const vetCatalogo = [
         preco: 142,
         imagem: 'camiseta-6.png',
         moletom: false,
+        modelo: 'camiseta'
     },
     {
         id: "7",
-        nome: 'CAMISETA "ACID LOVE" LOVE DELIVERY',
-        preco: 127,
+        nome: 'CAMISETA BAW x SMILEY NICE DAY',
+        preco: 151,
         imagem: 'camiseta-7.png',
         moletom: false,
+        modelo: 'camiseta'
     },
     {
         id: "8",
@@ -54,61 +61,435 @@ export const vetCatalogo = [
         preco: 139,
         imagem: 'camiseta-8.png',
         moletom: false,
+        modelo: 'camiseta'
     },
     {
         id: "9",
         nome: 'CAMISETA "GRAFF" KUNX',
-        preco: 131,
+        preco: 136,
         imagem: 'camiseta-9.png',
         moletom: false,
+        modelo: 'camiseta'
     },
     {
         id: "10",
+        nome: 'CAMISETA "ASSINATURA" BCREW',
+        preco: 129,
+        imagem: 'camiseta-10.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "11",
+        nome: 'CAMISETA "B TRIBAL" BCREW ',
+        preco: 141,
+        imagem: 'camiseta-11.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "12",
+        nome: 'CAMISETA "B GARÇA REMIX" BCREW',
+        preco: 136,
+        imagem: 'camiseta-12.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "13",
+        nome: 'CAMISETA "BERÇO B" BCREW',
+        preco: 136,
+        imagem: 'camiseta-13.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "14",
+        nome: 'CAMISETA "ACID LOVE" LOVE DELIVERY',
+        preco: 129,
+        imagem: 'camiseta-14.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "15",
         nome: 'BLUSA "L.T.H.T" ALLGLORY',
         preco: 198,
         imagem: 'blusa-1.png',
         moletom: false,
+        modelo: 'blusa'
     },
     {
-        id: "11",
+        id: "16",
         nome: 'BLUSA "D ROSES" DIAMOND',
         preco: 176,
         imagem: 'blusa-2.png',
         moletom: false,
+        modelo: 'blusa'
     },
     {
-        id: "12",
+        id: "17",
         nome: 'BLUSA "POSTING LESS" SOMETIMES',
         preco: 119,
         imagem: 'blusa-3.png',
         moletom: false,
+        modelo: 'blusa'
     },
     {
-        id: "13",
+        id: "18",
         nome: 'BLUSA "LUCK" SOMETIMES ',
         preco: 121,
         imagem: 'blusa-4.png',
         moletom: false,
+        modelo: 'blusa'
     },
     {
-        id: "14",
+        id: "19",
         nome: 'MOLETOM "DEATH ANGEL" DIAMOND ',
         preco: 259,
         imagem: 'moletom-1.png',
         moletom: true,
+        modelo: 'moletom'
     },
     {
-        id: "15",
+        id: "20",
         nome: 'MOLETOM "CLASS INVERSO" CLASS ',
         preco: 229,
         imagem: 'moletom-2.png',
         moletom: true,
+        modelo: 'moletom'
     },
     {
-        id: "16",
-        nome: 'MOLETOM "GOODS LOGO ÓRBITA" BC ',
+        id: "21",
+        nome: 'MOLETOM "GOODS LOGO ÓRBITA" BCREW ',
         preco: 259,
         imagem: 'moletom-3.png',
         moletom: true,
+        modelo: 'moletom'
+    },
+    {
+        id: "22",
+        nome: 'MOLETOM "BREAKING & ENTERING" DEATHWISH ',
+        preco: 259,
+        imagem: 'moletom-4.png',
+        moletom: true,
+        modelo: 'moletom'
+    }
+];
+
+export const vetAcessorios = [
+    {
+        id: "1",
+        nome: 'CAMISETA "THINK" HIGH',
+        preco: 142,
+        imagem: 'camiseta-1.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "2",
+        nome: 'CAMISETA "RAZOR" HIGH',
+        preco: 142,
+        imagem: 'camiseta-2.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "3",
+        nome: 'CAMISETA "CELLPHONE" HIGH',
+        preco: 142,
+        imagem: 'camiseta-3.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "4",
+        nome: 'CAMISETA "GOLF" HIGH',
+        preco: 153,
+        imagem: 'camiseta-4.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "5",
+        nome: 'CAMISA POLO SPORT HIGH',
+        preco: 168,
+        imagem: 'camiseta-5.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "6",
+        nome: 'CAMISETA "COMET" HIGH',
+        preco: 142,
+        imagem: 'camiseta-6.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "7",
+        nome: 'CAMISETA BAW x SMILEY NICE DAY',
+        preco: 151,
+        imagem: 'camiseta-7.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "8",
+        nome: 'CAMISETA "CINZEIRO" KUNX',
+        preco: 139,
+        imagem: 'camiseta-8.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "9",
+        nome: 'CAMISETA "GRAFF" KUNX',
+        preco: 136,
+        imagem: 'camiseta-9.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "10",
+        nome: 'CAMISETA "ASSINATURA" BCREW',
+        preco: 129,
+        imagem: 'camiseta-10.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "11",
+        nome: 'CAMISETA "B TRIBAL" BCREW ',
+        preco: 141,
+        imagem: 'camiseta-11.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "12",
+        nome: 'CAMISETA "B GARÇA REMIX" BCREW',
+        preco: 136,
+        imagem: 'camiseta-12.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "13",
+        nome: 'CAMISETA "BERÇO B" BCREW',
+        preco: 136,
+        imagem: 'camiseta-13.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "14",
+        nome: 'CAMISETA "ACID LOVE" LOVE DELIVERY',
+        preco: 129,
+        imagem: 'camiseta-14.png',
+        moletom: false,
+        modelo: 'camiseta'
+    },
+    {
+        id: "15",
+        nome: 'BLUSA "L.T.H.T" ALLGLORY',
+        preco: 198,
+        imagem: 'blusa-1.png',
+        moletom: false,
+        modelo: 'blusa'
+    },
+    {
+        id: "16",
+        nome: 'BLUSA "D ROSES" DIAMOND',
+        preco: 176,
+        imagem: 'blusa-2.png',
+        moletom: false,
+        modelo: 'blusa'
+    },
+    {
+        id: "17",
+        nome: 'BLUSA "POSTING LESS" SOMETIMES',
+        preco: 119,
+        imagem: 'blusa-3.png',
+        moletom: false,
+        modelo: 'blusa'
+    },
+    {
+        id: "18",
+        nome: 'BLUSA "LUCK" SOMETIMES ',
+        preco: 121,
+        imagem: 'blusa-4.png',
+        moletom: false,
+        modelo: 'blusa'
+    },
+    {
+        id: "19",
+        nome: 'MOLETOM "DEATH ANGEL" DIAMOND ',
+        preco: 259,
+        imagem: 'moletom-1.png',
+        moletom: true,
+        modelo: 'moletom'
+    },
+    {
+        id: "20",
+        nome: 'MOLETOM "CLASS INVERSO" CLASS ',
+        preco: 229,
+        imagem: 'moletom-2.png',
+        moletom: true,
+        modelo: 'moletom'
+    },
+    {
+        id: "21",
+        nome: 'MOLETOM "GOODS LOGO ÓRBITA" BCREW ',
+        preco: 259,
+        imagem: 'moletom-3.png',
+        moletom: true,
+        modelo: 'moletom'
+    },
+    {
+        id: "22",
+        nome: 'MOLETOM "BREAKING & ENTERING" DEATHWISH ',
+        preco: 259,
+        imagem: 'moletom-4.png',
+        moletom: true,
+        modelo: 'moletom'
+    }
+];
+
+export const vetSneakers = [
+    {
+        id: 1,
+        nome: 'NIKE "ORANGE MARINE" DUNK LOW SP',
+        preco: 384,
+        imagem: 'sneaker-1.png'
+    },
+    {
+        id: 2,
+        nome: 'NIKE "LX WHITE" BLAZER MID 77',
+        preco: 461,
+        imagem: 'sneaker-2.png'
+    },
+    {
+        id: 3,
+        nome: 'ÖUS IMIGRANTE NOTÓRIO',
+        preco: 297,
+        imagem: 'skate-1.png'
+    },
+    {
+        id: 4,
+        nome: 'SÖUSA NBA NY KNICKS ',
+        preco: 629,
+        imagem: 'sneaker-3.png'
+    },
+    {
+        id: 5,
+        nome: 'TESLA COIL WHITE PURPLE',
+        preco: 410,
+        imagem: 'skate-2.png'
+    },
+    {
+        id: 6,
+        nome: 'DC SHOES CONSTRUCT IMP',
+        preco: 379,
+        imagem: 'sneaker-4.png'
+    },
+    {
+        id: 7,
+        nome: 'DC SHOES KALYNX ZERO IMP',
+        preco: 350,
+        imagem: 'sneaker-5.png'
+    },
+    {
+        id: 8,
+        nome: 'DC x THRASHER TRUTH',
+        preco: 529,
+        imagem: 'sneaker-6.png'
+    },
+    {
+        id: 9,
+        nome: 'ADIDAS FORUM LOW',
+        preco: 439,
+        imagem: 'sneaker-7.png'
+    },
+    {
+        id: 10,
+        nome: 'REBOOK x BAW "REVENGE" CLUB C',
+        preco: 679,
+        imagem: 'sneaker-8.png'
+    }
+];
+
+const vetDecks = [
+    {
+        id: 1,
+        nome: 'SHAPE "ROADSTER" PRIMITIVE',
+        preco: 283,
+        imagem: 'deck-1.png'
+    },
+    {
+        id: 2,
+        nome: 'SHAPE "FOREVER TEAM" PRIMITIVE',
+        preco: 283,
+        imagem: 'deck-2.png'
+    },
+    {
+        id: 3,
+        nome: 'SHAPE "THIS WAY" DEATHWISH',
+        preco: 271,
+        imagem: 'deck-3.png'
+    },
+    {
+        id: 4,
+        nome: 'SHAPE "M. INFERNO" CREATURE',
+        preco: 298,
+        imagem: 'deck-4.png'
+    },
+    {
+        id: 5,
+        nome: 'SHAPE "CATACOMB" CREATURE',
+        preco: 242,
+        imagem: 'deck-5.png'
+    },
+    {
+        id: 6,
+        nome: 'SHAPE "AP OG LOGO" APRIL',
+        preco: 275,
+        imagem: 'deck-6.png'
+    },
+    {
+        id: 7,
+        nome: 'SHAPE "JEANNE" HOCKEY',
+        preco: 275,
+        imagem: 'deck-7.png'
+    },
+    {
+        id: 8,
+        nome: 'SHAPE "PREDICTIONS" FUCKING AWESOME',
+        preco: 289,
+        imagem: 'deck-8.png'
+    },
+    {
+        id: 9,
+        nome: 'LIXA ULTRAGRIP JESSUP',
+        preco: 89,
+        imagem: 'lixa-1.png'
+    },
+    {
+        id: 10,
+        nome: 'LIXA "OG SPRAY" SHAKE JUNT',
+        preco: 189,
+        imagem: 'lixa-2.png'
+    },
+    {
+        id: 11,
+        nome: 'LIXA "STAMP PRINT" GRIZZLY',
+        preco: 189,
+        imagem: 'lixa-3.png'
+    },
+    {
+        id: 12,
+        nome: 'LIXA "BIGHEAD INFERNO" SPITFIRE',
+        preco: 199,
+        imagem: 'lixa-4.png'
     }
 ];
