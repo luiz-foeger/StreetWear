7ff4ff089b82b252721e02b73872b335efc2eab7
Luiz Felipe Föeger <luizffsan5@gmail.com>
2025-05-09 15:21:29 -0300

    Adição de media-query para o header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,13 @@ import { useEffect, useState } from "react";
 // Import Icons
 import { RiArrowDownSLine } from "react-icons/ri";
 import { IoBagHandle, IoMenu } from "react-icons/io5";
+import { TfiClose } from "react-icons/tfi";
 
 import estilos from './Header.module.css';
 
 function Header() {
     const [rolado, setRolado] = useState(false);
+    const [mostrarModal, setMostrarModal] = useState(false);
 
     useEffect(() => {
         const aoRolar = () => {
@@ -19,27 +21,50 @@ function Header() {
     }, []);
 
     return (
-        <header className={`${estilos.header} ${rolado ? estilos.rolado : ""}`}>
-            <img to="/" className={estilos.logo} src="/assets/images/logos/logo.png" alt="Logo" />
-            <nav className={estilos.nav}>
-                <h3 className={estilos.menu}>
-                    <Link to="/">Início</Link>
+        <>
+            <header className={`${estilos.header} ${rolado ? estilos.rolado : ""}`}>
+                <Link to="/" className={`${estilos.logo} ${estilos.divLogo}`}><img className={estilos.logo} src="/assets/images/logos/logo.png" alt="Logo" /></Link>
 
-                    <div className={estilos.opcaoDropdown}>
-                        <Link to="#">Produtos <RiArrowDownSLine /></Link>
-                        <ul className={estilos.dropdownProdutos}>
-                            <li><Link to="/produtos/acessorios" style={{ marginTop: "20px" }}>Acessórios</Link></li>
-                            <li><Link to="/produtos/vestuario">Vestuário</Link></li>
-                            <li><Link to="/produtos/sneakers">Sneakers</Link></li>
-                            <li><Link to="/produtos/skateboard">Skate</Link></li>
-                        </ul>
+                <Link className={`${estilos.iconeMobile} ${rolado ? estilos.rolado : ""}`}><IoMenu onClick={() => setMostrarModal(true)} /></Link>
+                <Link to="/" className={estilos.logoMobile}><img className={estilos.logoMobile} src="/assets/images/logos/logo2.png" alt="Logo" /></Link>
+
+
+                <nav className={estilos.nav}>
+                    <h3 className={estilos.menu}>
+                        <Link to="/">Início</Link>
+
+                        <div className={estilos.opcaoDropdown}>
+                            <Link to="#">Produtos <RiArrowDownSLine /></Link>
+                            <ul className={estilos.dropdownProdutos}>
+                                <li><Link to="/produtos/acessorios" style={{ marginTop: "20px" }}>Acessórios</Link></li>
+                                <li><Link to="/produtos/vestuario">Vestuário</Link></li>
+                                <li><Link to="/produtos/sneakers">Sneakers</Link></li>
+                                <li><Link to="/produtos/skateboard">Skate</Link></li>
+                            </ul>
+                        </div>
+
+                        <Link to="/sobre-nos">Sobre</Link>
+                    </h3>
+                </nav>
+
+                <Link className={`${estilos.icone} ${rolado ? estilos.rolado : ""}`} to="/sacola"><IoBagHandle /></Link>
+            </header>
+            {
+                mostrarModal && (
+                    <div className={estilos.menuOverlay} onClick={() => setMostrarModal(false)}>
+                        <div className={estilos.menuContent} onClick={(e) => e.stopPropagation()}>
+                            <ul onClick={() => setMostrarModal(false)}>
+                                <li><Link to="/produtos/acessorios">Acessórios</Link></li>
+                                <li><Link to="/produtos/vestuario">Vestuário</Link></li>
+                                <li><Link to="/produtos/sneakers">Sneakers</Link></li>
+                                <li><Link to="/produtos/skateboard">Skate</Link></li>
+                            </ul>
+                        </div>
+                            {/* <button className={estilos.botaoFechar} onClick={() => setMostrarModal(false)}>Fechar</button> */}
                     </div>
-
-                    <Link to="/sobre-nos">Sobre</Link>
-                </h3>
-            </nav>
-            <Link className={estilos.icone} to="/sacola"><IoBagHandle /></Link>
-        </header>
+                )
+            }
+        </>
     );
 }
 
