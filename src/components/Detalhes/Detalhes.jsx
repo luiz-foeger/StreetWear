6990e60de26a399914031c981b8d89399602ed8a
Luiz Felipe Föeger <luizffsan5@gmail.com>
2025-05-30 00:57:04 -0300

    Ajustando descrição dos produtos por cada categoria

--- src/components/Detalhes/Detalhes.jsx
+++ src/components/Detalhes/Detalhes.jsx
@@ -36,6 +36,21 @@ function ProdutoDetalhes() {
 
     let titulo = `${item.modelo} ${item.marca} ${item.nome}`;
 
+    const descricoesPorModelo = {
+        camiseta: "Camiseta 100% algodão com 270 g/m² \nModelagem Working Title \nArte em silk screen \nEtiquetas ",
+        moletom: "Moletom 50% Algodão, \n 50% Poliéster \n Bordado / Embroidered Logo \n Etiquetas ",
+        jaqueta: "Jaqueta 100% algodão \n Bordado / Embroidered Logo \n Etiquetas ",
+        boné: "90% Poliéster, \n 10% Poliamida \n Arte em plastisol \n Etiquetas e metais ",
+        gorro: "70% poliéster, \n 30% acrílico Team Model \n Tamanho único \n Etiquetas ",
+        jorts: "Jeans 100% Algodão \n  Rebites personalizados \n Fechamento de botão e zíper YKK \n Etiquetas ",
+        bermuda: "Shorts com ajuste atlético \n Elastic Waistband \n Etiquetas ",
+        sneakers: "Material sintético de alta qualidade, solado antiderrapante, palmilha confortável, design moderno \n Colorway Original ",
+        shape: "8.0” x 31.5” \n 14.25” Wheelbase \n DW Deep Concave \n 100% Canadian Maple ",
+        lixa: "Tam. 33x9” \n Comprimento: 84cm \n Largura: 22,5cm \n Lixa de alta abrasão ",
+    };
+
+    const descricaoProduto = descricoesPorModelo[item.modelo.toLowerCase()] || "    ";
+
     return (
         <>
             <div className={estilos.indicadorPagina}>
@@ -50,13 +65,11 @@ function ProdutoDetalhes() {
 
                 <div className={estilos.detalhes}>
                     <h1>{titulo.toUpperCase()}</h1>
-
                     <h2>R$ {item.preco}</h2>
+
                     <h3>COMPOSIÇÃO</h3>
-                    <p>{capitalize(item.modelo)} 100% algodão com 270 g/m² <br />
-                        Modelagem Working Title  <br />
-                        Arte em silk screen <br />
-                        Etiquetas {item.marca}®</p>
+                    <p>{`${descricaoProduto}${item.marca}®`}</p>
+
                     <select id="select" className={estilos.select} name="select" required>
                         <option style={{ fontWeight: '200' }} value="selecionar" selected disabled>Tamanho</option>
                         <option style={{ fontWeight: '200' }} value="PP">PP</option>
